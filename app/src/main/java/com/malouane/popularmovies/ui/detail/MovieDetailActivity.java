3e42c046cffb492371f480ffe0f164f3fbefe311
Mohamed Alouane <flahweb@gmail.com>
2017-10-09 22:42:14 +0100

    show fav FAB

--- app/src/main/java/com/malouane/popularmovies/ui/detail/MovieDetailActivity.java
+++ app/src/main/java/com/malouane/popularmovies/ui/detail/MovieDetailActivity.java
@@ -10,15 +10,18 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.malouane.popularmovies.R;
+import com.malouane.popularmovies.data.entity.MovieTrailerEntity;
 import com.malouane.popularmovies.databinding.ActivityMovieDetailBinding;
+import com.malouane.popularmovies.utils.PopMovisUtils;
 
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjection;
 
-public class MovieDetailActivity extends AppCompatActivity implements LifecycleRegistryOwner {
+public class MovieDetailActivity extends AppCompatActivity implements LifecycleRegistryOwner, TrailerListCallback {
 
   private static final String KEY_MOVIE_ID = "KEY_MOVIE_ID";
   private static final String KEY_MOVIE_POSTER = "KEY_MOVIE_POSTER";
@@ -29,6 +32,7 @@ public class MovieDetailActivity extends AppCompatActivity implements LifecycleR
   @Inject MovieDetailViewModel movieDetailViewModel;
 
   MovieReviewsAdapter movieReviewsAdapter;
+  MovieTrailersAdapter movieTrailersAdapter;
 
   public static Intent newIntent(Context context, int movieID, String posterPath) {
     Intent intent = new Intent(context, MovieDetailActivity.class);
@@ -63,9 +67,37 @@ public class MovieDetailActivity extends AppCompatActivity implements LifecycleR
                 .observe(this, movie -> binding.setMovie(movie));
       }
     }
+
+    movieReviewsAdapter = new MovieReviewsAdapter();
+    movieTrailersAdapter = new MovieTrailersAdapter(this);
+
+    binding.rvMovieTrailers.setAdapter(movieTrailersAdapter);
+    binding.rvMovieReviews.setAdapter(movieReviewsAdapter);
+
+    getReviews();
+    getTrailers();
+  }
+
+  private void getTrailers() {
+    binding.setShowLoading(true);
+    movieDetailViewModel.getMovieTrailers()
+            .observe(this, list -> {
+              binding.setShowLoading(false);
+              binding.setTrailersList(list);
+            });
+  }
+
+  private void getReviews() {
+    binding.setShowLoading(true);
+    movieDetailViewModel.getMovieReviews()
+            .observe(this, list -> {
+              binding.setShowLoading(false);
+              binding.setReviewsList(list);
+            });
   }
 
   private void initFAB() {
+    binding.favoriteFab.setVisibility(View.VISIBLE);
     binding.favoriteFab.setOnClickListener(view1 ->
             movieDetailViewModel.performFavorites(binding.getMovie()).observe(this, aBoolean -> movieDetailViewModel.retry()));
   }
@@ -81,5 +113,15 @@ public class MovieDetailActivity extends AppCompatActivity implements LifecycleR
 
   @Override public LifecycleRegistry getLifecycle() {
     return lifecycleRegistry;
+  }
+
+  @Override
+  public void onTrailerClicked(MovieTrailerEntity trailerEntity) {
+    PopMovisUtils.buildYouTubeLink(trailerEntity.getKey());
+  }
+
+  @Override
+  public void onTrailerShareClicked(String ytKey) {
+    PopMovisUtils.shareTrailer(this, ytKey);
   }
 }
